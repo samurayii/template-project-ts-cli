a98d24bae99ac71adb34883552ccbe7c8e11212c
Alexandr <samurayiii1986@gmail.com>
2021-04-16 20:07:09 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 import config from "./lib/entry";
+import { ILogger, Logger } from "logger-flx";
 
 console.log(config);
+
+let logger: ILogger;
+
+if (config.logs === "none") {
+    logger = new Logger({
+        mode: "prod",
+        type: true,
+        timestamp: "none",
+        enable: false
+    });
+} else {
+    logger = new Logger({
+        mode: config.logs,
+        type: true,
+        timestamp: "none",
+        enable: true
+    });
+}
+
+logger.log("start");
--- src/lib/entry.ts
+++ src/lib/entry.ts
@@ -1,32 +1,64 @@
-import { program } from "commander";
-//import * as chalk from "chalk";
-//import * as fs from "fs";
-//import * as path from "path";
-import * as finder from "find-package-json";
-//import * as Ajv from "ajv";
-//import jtomler from "jtomler";
-//import json_from_schema from "json-from-default-schema";
-//import * as config_schema from "./schemes/config.json";
- 
-const pkg = finder(__dirname).next().value;
+import { Command } from "commander";
+import * as chalk from "chalk";
+import * as fs from "fs";
+import * as path from "path";
+import { IAppConfig } from "./interfaces";
 
-const config = {};
+type TPackage = {
+    [key: string]: unknown
+}
 
-program.version(`version: ${pkg.version}`, "-v, --version", "output the current version.");
-program.name(pkg.name);
-program.option("-c, --config <type>", "Path to config file. If not set, searching configuration in package.json file.");
+const findPkg = (): TPackage => {
+
+    const cwd_pkg_full_path = path.resolve(process.cwd(), "package.json");
+    const dirname_pkg_full_path = path.resolve(__dirname, "package.json");
+    const app_pkg_full_path = path.resolve(path.dirname(process.argv[1]), "package.json");
+    const require_pkg_full_path = path.resolve(path.dirname(require.main.filename), "package.json"); 
+
+    if (fs.existsSync(dirname_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(dirname_pkg_full_path).toString());
+    }
+    if (fs.existsSync(app_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(app_pkg_full_path).toString());
+    }
+    if (fs.existsSync(require_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(require_pkg_full_path).toString());
+    }   
+    if (fs.existsSync(cwd_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(cwd_pkg_full_path).toString());
+    }
+
+};
+
+const program = new Command();
+const pkg = findPkg();
+
+if (pkg === undefined) {
+    console.error(chalk.red("[ERROR] package.json not found"));
+    process.exit(1);
+}
+
+program.version(`${pkg.name} version: ${pkg.version}`, "-v, --version", "output the current version.");
+program.name(<string>pkg.name);
+program.option("-l, --logs <type>", "Logs details, can be none, none, prod, dev or debug (Environment variable: TEMPLATE_LOGS=<type>). Example: --logs prod", "prod");
+
+
+
+
+
+
+
 
 program.parse(process.argv);
 
+const options = program.opts();
 
-/*
-const ajv = new Ajv();
-const validate = ajv.compile(config_schema);
+const config: IAppConfig = {
+    logs: options.logs.trim().toLowerCase()
+};
 
-if (!validate(config)) {
-    console.error(chalk.red(`[ERROR] Schema errors:\n${JSON.stringify(validate.errors, null, 2)}`));
-    process.exit(1);
+if (process.env["TEMPLATE_LOGS"] !== undefined) {
+    config.logs = process.env["TEMPLATE_LOGS"].trim();
 }
-*/
 
 export default config;
